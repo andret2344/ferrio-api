ae949e05eea35ca3d106b86edb04eaf8513727ee
Andret2344 <andret2344@gmail.com>
2021-12-04 22:07:40 +0100

    Added url to language

--- src/Entity/Language.php
+++ src/Entity/Language.php
@@ -12,17 +12,20 @@ class Language implements JsonSerializable {
 	#[ORM\Column(type: 'string', length: 31)]
 	private string $code;
 
-	#[ORM\Id]
-	#[ORM\Column(type: 'string', length: 63)]
+	#[ORM\Column(type: 'string', length: 63, unique: true)]
 	private string $name;
 
 	#[ORM\Column(type: 'integer')]
 	private int $releaseId;
 
-	public function __construct(string $code, string $name, string $releaseId) {
+	#[ORM\Column(type: 'string', unique: true)]
+	private string $url;
+
+	public function __construct(string $code, string $name, string $releaseId, string $url) {
 		$this->code = $code;
 		$this->name = $name;
 		$this->releaseId = $releaseId;
+		$this->url = $url;
 	}
 
 	public function getCode(): string {
@@ -37,12 +40,17 @@ class Language implements JsonSerializable {
 		return $this->releaseId;
 	}
 
+	public function getUrl(): string {
+		return $this->url;
+	}
+
 	#[ArrayShape([
-		'uniLanguage' => "string",
-		'language' => "string",
-		'code' => "string",
-		'name' => "string",
-		'releaseId' => "int|string"
+		'uniLanguage' => 'string',
+		'language' => 'string',
+		'code' => 'string',
+		'name' => 'string',
+		'releaseId' => 'int',
+		'url' => 'string'
 	])]
 	public function jsonSerialize(): array {
 		return [
@@ -50,7 +58,8 @@ class Language implements JsonSerializable {
 			'language' => $this->name,
 			'code' => $this->code,
 			'name' => $this->name,
-			'releaseId' => $this->releaseId
+			'releaseId' => $this->releaseId,
+			'url' => $this->url
 		];
 	}
 }
