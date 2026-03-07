51323ed48c7c2e06dc0afcb5c63f5d03702b6fa5
Andret2344 <andret2344@gmail.com>
2026-03-07 15:32:35 +0100

    Fixed dev token verifier

--- src/Service/DevFirebaseTokenVerifier.php
+++ src/Service/DevFirebaseTokenVerifier.php
@@ -8,11 +8,20 @@ class DevFirebaseTokenVerifier extends FirebaseTokenVerifier
 {
 	public function __construct()
 	{
+		// empty
 	}
 
 	#[Override]
 	public function verify(string $token): string
 	{
+		$parts = explode('.', $token);
+		if (count($parts) === 3) {
+			$payload = json_decode(base64_decode($parts[1]), true);
+			if (isset($payload['sub'])) {
+				return $payload['sub'];
+			}
+		}
+
 		return $token;
 	}
 
